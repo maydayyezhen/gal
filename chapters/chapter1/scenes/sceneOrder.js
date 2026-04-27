ddff83531b2076ef0c0cefc837d9530d6853b99e
Zhang Zihao <162564657+maydayyezhen@users.noreply.github.com>
2026-04-27 22:57:59 +0800

    refactor: add chapter1 scene order

---
+++ chapters/chapter1/scenes/sceneOrder.js
@@ -0,0 +1,12 @@
+window.CHAPTER1_SCENE_ORDER = [
+  'scene_ch1_classroom',
+  'scene_ch1_classroom_2',
+  'scene_ch1_classroom_choice1',
+  'scene_ch1_classroom_choice2',
+  'scene_ch1_home',
+  'scene_ch1_dream_start',
+  'scene_ch1_dream_princess',
+  'scene_ch1_dream_princess_2',
+  'scene_ch1_dream_accept',
+  'scene_ch1_dream_hesitate'
+];
