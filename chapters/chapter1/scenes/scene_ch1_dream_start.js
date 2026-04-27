ee74c3c9b40a52fc8edb5fbf556e7703cc9b222f
Zhang Zihao <162564657+maydayyezhen@users.noreply.github.com>
2026-04-27 22:19:34 +0800

    refactor: split scene_ch1_dream_start

---
+++ chapters/chapter1/scenes/scene_ch1_dream_start.js
@@ -0,0 +1,52 @@
+window.CHAPTER1_SCENES = window.CHAPTER1_SCENES || {};
+window.CHAPTER1_SCENES.scene_ch1_dream_start = {
+  "id": "scene_ch1_dream_start",
+  "background": "bg_forest",
+  "bgm": "bgm_dream_forest",
+  "particles": "firefly",
+  "characters": [
+    {
+      "id": "char_sora_surprised",
+      "position": "center",
+      "opacity": 1
+    }
+  ],
+  "dialogs": [
+    {
+      "speaker": "",
+      "text": "……嗯?",
+      "effects": {
+        "eyeOpen": {
+          "duration": 520,
+          "blur": 1.3
+        }
+      }
+    },
+    {
+      "speaker": "",
+      "text": "我睁开了眼,发现自己站在一片奇怪的森林里。"
+    },
+    {
+      "speaker": "",
+      "text": "周围的树发着淡淡的蓝光,空气中飘着像萤火虫一样的光点。"
+    },
+    {
+      "speaker": "夜",
+      "text": "这……做梦了?"
+    },
+    {
+      "speaker": "",
+      "text": "但这梦也太清晰了。我能感觉到脚下草地的触感,闻到花香,甚至听到远处的水声。"
+    },
+    {
+      "speaker": "夜",
+      "text": "好吧,既然是梦,那就随便走走。"
+    },
+    {
+      "speaker": "",
+      "text": "我沿着小路往前走,前方传来了……哭声?"
+    }
+  ],
+  "choices": null,
+  "autoNext": "scene_ch1_dream_princess"
+};
